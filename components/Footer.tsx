bde5230fbbf902a9ef5d3a723a5bc9f69f71a465
JOduMonT <5204724+JOduMonT@users.noreply.github.com>
2025-02-03 01:27:01 +0100

    custom foot

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -36,7 +36,8 @@ export function FooterImpl() {
   return (
     <footer className={styles.footer}>
       <div className={styles.copyright}>
-        Copyright {currentYear} {config.author}
+      {config.name} © 2004-{currentYear} by {config.author}<br/>
+      Made Everywhere all at Once @ Anywhere Out of the World.
       </div>
 
       <div className={styles.settings}>
